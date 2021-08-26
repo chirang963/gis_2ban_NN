b5fe175dc7948dcddd3c7d8e6dc80b706893ae46
qawsedrf963 <qawsedrf963@naver.com>
2021-08-26 17:52:56 +0900

    8/26 ban class commit 2

--- temp/settings.py
+++ temp/settings.py
@@ -168,5 +168,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
