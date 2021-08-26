0a955ea9022118d4115a30c071d37a0d018a4e69
qawsedrf963 <qawsedrf963@naver.com>
2021-08-26 18:10:29 +0900

    8/26 ban class commit 4

--- temp/urls.py
+++ temp/urls.py
@@ -1,5 +1,4 @@
-"""temp URL Configuration
-
+"""gis_2ban_1 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -15,18 +14,22 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib import admin
-
+from articleapp.views import ArticleListView
 
 urlpatterns = [
+    path('', ArticleListView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+
     path('subscribe/', include('subscribeapp.urls')),
     path('likes/', include('likeapp.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
